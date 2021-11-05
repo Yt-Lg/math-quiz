eb934d51513d25ab023f7ac388e8f2def4642c27
YL <67716376+Yt-Lg@users.noreply.github.com>
2021-11-05 16:25:45 +0000

    Add files via upload

---
+++ Math Quiz/Math Quiz/Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Math_Quiz
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> symbol = new List<string>() { "+", "-", "*", "/" };
+
+            Console.WriteLine("input name");
+            string NameInput = Console.ReadLine();
+
+            Console.WriteLine("input difficulty ( Easy | Medium | Hard )");
+            string Difficulty = Console.ReadLine();
+
+            if (Difficulty == "Easy")
+            {
+                while(true)
+                {
+                    Random random = new Random();
+                    for (int i = 0; i < 10; i++)
+                    {
+                        int num1 = random.Next(1, 5);
+                    }
+
+                   
+
+                }
+            }
+
+            else if (Difficulty == "Medium")
+            {
+                Random random = new Random();
+                for (int i = 0; i < 20; i++)
+                {
+                    int num1 = random.Next(1, 5);
+                }
+
+
+
+            }
+
+            else if (Difficulty == "Hard")
+            {
+                Random random = new Random();
+                for (int i = 0; i < 30; i++)
+                {
+                    int num1 = random.Next(1, 5);
+                }
+
+
+
+            }
+
+            else
+            {
+                Console.WriteLine("invalid input");
+            }
+
+        }
+
+    }
+}
+
